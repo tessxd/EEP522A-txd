3f1297d3f0a55a680a006f59b32c8599ee8cb257
Tess <tessxd@gmail.com>
2019-02-04 07:48:54 +0000

    fixed SPITest.c

--- HW2/SPITest.c
+++ HW2/SPITest.c
@@ -1,16 +1,26 @@
 
 #include <stdio.h>
-#include <wiringPi.h>
+#include <errno.h>
+#include <string.h>
+#include <linux/spi/spidev.h>
 
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
 
 int main ()
 {
-    //Set up chip select to CE1
-    const int CHANNEL = 1;
+    //Set up chip select to CE0
+    const int CHANNEL = 0;
+	
+	//Set up buffer
+	unsigned char buf[100];
+	buf[0] = 0xff;
+	buf[1] = 0xff;
 
     //Setup SPI
     int setup;
-    setup = wiringPiSPISetup(CHANNEL, 500000);
+    setup = wiringPiSPISetup(CHANNEL, 8000000);
+    //wiringPiSetup ();
 
     //print result of setup
     printf("%d", setup);
@@ -19,10 +29,14 @@ int main ()
     while(1)
     {
         //write a random buffer to the PI
-        wiringPiSPIDataRW(CHANNEL, 0x58, 1);
+        wiringPiSPIDataRW(0, buf, 1);
 
         //delay between writes
-        sleep(5);
+        delay(100);
+        
+        //print a confirmation
+        printf("writing data");
+        printf("\n");
     }
 
     return 0;
