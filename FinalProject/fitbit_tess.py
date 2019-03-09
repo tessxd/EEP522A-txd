adef1ed2f443624069d8504b1447ab54e9908d9f
Tess <tdespres@hmc.edu>
2019-03-09 09:42:00 -0800

    added moisture sensor code

---
+++ FinalProject/fitbit_tess.py
@@ -0,0 +1,12 @@
+import fitbit
+import gather_keys_oauth2 as Oauth2
+import pandas as pd 
+import datetime
+CLIENT_ID = '22DKGQ'
+CLIENT_SECRET = '756f3f3f6b215ff65c260099aa5d0a13'
+
+server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+server.browser_authorize()
+ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
---
+++ FinalProject/readMoistureSensor.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+include <stdlib.h>
+include <wiringPi.h>
+
+int main (void)
+{
+    //set up libraries
+    wiringPiSetup();
+    //set pin as input
+    pinMode(22, INPUT);
+    //read in value
+    int value = digitalRead(22);
+    //print to console to ensure it's working
+    printf("%d", value);
+}
